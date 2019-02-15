6d897d37d6e00421a625735e860ec1b7cd3f76de
sindhusha-t <sindhusha55@gmail.com>
2019-02-15 09:38:03 +0530

    1) Added Bootstrap CSS file to index.html file 2) Added Home, Login, Register Components 3) Added app Routing file 4) Added basic html content for all components 5) Pointed the declared components in the routing app file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+import { routing} from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.ts
@@ -0,0 +1,12 @@
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+
+const appRoutes: Routes = [
+  { path: '', component: LoginComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent}
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
