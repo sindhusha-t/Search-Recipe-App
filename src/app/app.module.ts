f52ffb498971a6a4271e67de75d85fa6d37890e9
sindhusha-t <sindhusha55@gmail.com>
2019-02-15 20:28:04 +0530

    1) Adding validation service and control message component. 2) Added Validation to register form.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StorageServiceModule} from 'ngx-webstorage-service';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
 import { routing} from './app.routing';
+import {LocalStorageService} from './local-storage.service';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { ControlMessagesComponent } from './control-messages/control-messages.component';
+import {ValidationService} from './validation.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    HomeComponent
+    HomeComponent,
+    ControlMessagesComponent
   ],
   imports: [
     BrowserModule,
-    routing
+    routing,
+    StorageServiceModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [LocalStorageService, ValidationService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  private static HttpModule: any;
+}
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {LocalStorageService} from '../local-storage.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { ControlMessagesComponent} from '../control-messages/control-messages.component';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  userRegisterForm: FormGroup;
+  submittedForm = false;
+
+  constructor( private localStorage: LocalStorageService, private fb: FormBuilder) {}
+
 
   ngOnInit() {
+    this.userRegisterForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      emailID: ['', [Validators.required, Validators.email]],
+      userName: ['', [Validators.required, Validators.minLength(6)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  get f() { return this.userRegisterForm.controls; }
+
+  saveUser(): void {
+
+    this.submittedForm = true;
+    if( this.userRegisterForm.dirty && this.userRegisterForm.valid ) {
+      this.localStorage.StoreRegisterDetailsInLocalStorage(this.userRegisterForm);
+    }
   }
 
 }
